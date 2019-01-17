e10f65374c86db41555a8d3d4f4dd8c8da3720f4
Loys Ollivier <loys.ollivier@gmail.com>
2019-01-17 10:51:29 +0100

    spacemacs: layouts: auto-load last saved layouts
    Layouts are great for splitting workspaces. Autoload them at startup.
    
    Signed-off-by: Loys Ollivier <loys.ollivier@gmail.com>

--- init.el
+++ init.el
@@ -317,6 +317,8 @@ executes.
  This function is mostly useful for variables that need to be set
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
+  ;; Auto load last saved layouts
+  (setq dotspacemacs-auto-resume-layouts t)
   )
 
 (defun dotspacemacs/user-config ()
