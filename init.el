5c73d1f94df6c1e205b916b6d1e79d983b3df8cd
Loys Ollivier <loys.ollivier@gmail.com>
2019-03-25 14:33:07 +0100

    spacemacs & magit: use dotspacemacs-directory variable & transient
    Magit has switched to transient.
    Transient is using a file to store the git configuration. This is annoying
    but ok... Set this file to be stored in my config.
    
    Also use the dotspacemacs-directory var instead of hardcoding the config
    directory.
    
    Signed-off-by: Loys Ollivier <loys.ollivier@gmail.com>

--- init.el
+++ init.el
@@ -335,9 +335,9 @@ explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
 
   ;; User config encapsulated in org
-  (setq my-user-config-file "~/.spacemacs.d/user-config.org")
+  (setq my-user-config-file (concatenate 'string dotspacemacs-directory "/user-config.org"))
   (org-babel-load-file my-user-config-file)
-  (setq my-mail-config-file "~/.spacemacs.d/mail-config.org")
+  (setq my-mail-config-file (concatenate 'string dotspacemacs-directory "/mail-config.org"))
   (org-babel-load-file my-mail-config-file)
   )
 
---
+++ transient/values.el
@@ -0,0 +1,2 @@
+((magit-commit "--signoff")
+ (magit-log "-n64" "--color" "--decorate"))
