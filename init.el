055146575ef1d2b1eda0367df801ed3da6c7a5c3
Loys Ollivier <loys.ollivier@arm.com>
2020-03-27 08:36:21 -0500

    emacs: set a nice font
    Hack looks good.
    
    Signed-off-by: Loys Ollivier <loys.ollivier@arm.com>

--- init.el
+++ init.el
@@ -174,11 +174,11 @@ values."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Ubuntu Mono"
-                               :size 17
+   dotspacemacs-default-font '("Hack"
+                               :size 10.0
                                :weight normal
                                :width normal
-                               :powerline-scale 1.1)
+                               :powerline-scale 1.0)
    ;; The leader key
    dotspacemacs-leader-key "SPC"
    ;; The key used for Emacs commands (M-x) (after pressing on the leader key).
